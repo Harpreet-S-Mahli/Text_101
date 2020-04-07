366022d7c13087bf61217f11834bcda35e660277
Harpreet S. Mahli <mahli.harpreet@yahoo.com>
2020-04-06 18:33:33 -0700

    First commit with basic script set up

---
+++ Text101/Assets/AdventureGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    // Now can be manipulated in the inspector
+    [SerializeField] Text textComponent;
+    string wordsInTextComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wordsInTextComponent = "Wow";
+        textComponent.text = wordsInTextComponent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
